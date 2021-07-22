1e9a13e7dfc9ab59e788c297dbcde41d19e42509
Luis Eduardo Imbernón <imbernon@gmail.com>
2021-07-22 20:11:34 +0200

    Added the return object.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import request
 from flask_cors import CORS
 import preprocess
 import model
+import numpy as np
 
 app = Flask(__name__)
 CORS(app)
@@ -11,10 +12,24 @@ CORS(app)
 @app.route('/api/v1/help-model/predict', methods=['POST'])
 def predict():
     if request.method == "POST":
+
+        # Get the data and preprocess the data
         elements = request.data
         df = preprocess.data_transformation(elements)
+
+        # Predicts the output
         prediction = model.predict("model/help_model.h5", "model/selectedfeatures.csv", df)
-        return prediction
+        print(prediction)
+
+        # Round the prediction to integers
+        prediction_int = np.rint(prediction)
+
+        # Searches if there if the help must be shown
+        help = 1 in prediction_int
+        help_object = "{show_help: " + str(int(help)) + "}"
+        print(help_object)
+
+        return help_object
 
 
 if __name__ == "__main__":

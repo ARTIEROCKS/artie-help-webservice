fef18dc7ea536bf3f60e278317003860e9254281
Luis Eduardo Imbernón <imbernon@gmail.com>
2021-08-09 20:33:01 +0200

    Error management.

--- app.py
+++ app.py
@@ -12,22 +12,47 @@ CORS(app)
 def predict():
     if request.method == "POST":
 
-        # Get the data and searches for the student interactions
-        elements = request.data
-        student_interactions, client = queue_service.get_student_interactions(elements)
+        error_message = ""
+        student_interactions = None
+        df = None
+        prediction_int = None
 
-        # Once we have the interactions of the student, we transform the data to get a valid array
-        df = preprocess.data_transformation(student_interactions["interactions"])
+        try:
+            # Get the data and searches for the student interactions
+            elements = request.data
+            student_interactions, client = queue_service.get_student_interactions(elements)
+        except:
+            error_message = "Error: Predict - get_student_interactions"
+            print("Error: Predict - get_student_interactions")
 
-        # Predicts the output
-        prediction = model.predict("model/help_model.h5", "model/selectedfeatures.csv", df)
+        try:
+            if student_interactions is not None:
+                # Once we have the interactions of the student, we transform the data to get a valid array
+                df = preprocess.data_transformation(student_interactions["interactions"])
+        except:
+            error_message = error_message + " | Error: Predict - data_transformation"
+            print("Error: Predict - data_transformation")
 
-        # Round the prediction to integers
-        prediction_int = np.rint(prediction)
+        try:
+            if df is not None:
+                # Predicts the output
+                prediction = model.predict("model/help_model.h5", "model/selectedfeatures.csv", df)
 
-        # Searches if there if the help must be shown
-        help = 1 in prediction_int
-        help_object = "{\"body\": {\"message\": null, \"object\": " + str(int(help)) + "}}"
+                # Round the prediction to integers
+                prediction_int = np.rint(prediction)
+        except:
+            error_message = error_message + " | Error: Predict - prediction"
+            print("Error: Predict - prediction")
+
+        if prediction_int is not None:
+
+            # Searches if there if the help must be shown
+            help = 1 in prediction_int
+            help_object = "{\"body\": {\"message\": null, \"object\": " + str(int(help)) + "}}"
+
+        else:
+            # If there are no predictions, we return the errors
+            help_object = "{\"body\": {\"message\": \"" + error_message + "\", \"object\": null }}"
 
         return help_object
 
